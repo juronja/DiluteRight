fcb35810e94d92895459f8767b456ce62e5046ef
juronja <101410098+juronja@users.noreply.github.com>
2024-03-29 16:38:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DiluteRight
+
+A simple alcohol dilution calculator I have made for my own use with tinctures.
+
+## Docker setup
+
+1. Run container from image<br>
+```docker run -d -p 7474:80 --restart unless-stopped --name dilute-right juronja/dilute-right:latest```
+
+
+## Favicon provided by:
+Beaker by Dan Draper from <a href="https://thenounproject.com/browse/icons/term/beaker/" target="_blank" title="Beaker Icons">Noun Project</a> (CC BY 3.0)
---
+++ app/app.js
@@ -0,0 +1,31 @@
+function handleImgLoad() {
+    const nrImages = 22
+    const randomNumber = Math.floor(Math.random() * nrImages) + 1;
+    const newImgSrc = "assets/images/cat" + randomNumber + ".webp"
+
+    document.getElementById("random-cat").querySelector("img").src = newImgSrc
+}
+
+
+function handleCalculate() {
+    let targetVolume = document.getElementById("target-volume").value;
+    let ratio = document.getElementById("ratio").value;
+    let alcTarget = document.getElementById("alcohol-target").value;
+    let alcSource = document.getElementById("alcohol-source").value;
+
+    // Calculate
+    let herbs = Math.round(targetVolume / ratio);
+    let alcohol = Math.round(targetVolume * alcTarget / alcSource);
+    let water = Math.round(targetVolume - targetVolume * alcTarget / alcSource);
+
+    // Print results
+    if (targetVolume !== "") {
+        document.getElementById("results-output").style.display = "block";
+        document.getElementById("result-validation").style.display = "none";
+        document.getElementById("herbs").textContent = "~ " + herbs + " g";
+        document.getElementById("alcohol").textContent = alcohol + " ml";
+        document.getElementById("water").textContent = water + " ml";
+    } else {
+        document.getElementById("result-validation").style.display = "block";
+    }
+}
