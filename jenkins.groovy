e87a521e1d3047fc6a871afde2f965c97c94bab6
juronja <101410098+juronja@users.noreply.github.com>
2024-10-02 18:30:35 +0200

    added define

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,12 +1,12 @@
 def buildEC2() {
     // Check if container exists
-    echo "piza" +
-    echo "lala"
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@35.157.110.150 'def containerId = sh(script: "docker ps --quiet --filter name=$CONTAINER_NAME", returnStdout: true).trim()'"
+    
     }
-    //sh "ssh -o StrictHostKeyChecking=no ec2-user@35.157.110.150 $DOCKER_RUN"
+    
 
-//                    def containerId = sh(script: "docker ps --quiet --filter name=$CONTAINER_NAME", returnStdout: true).trim()
-//
+
+//$DOCKER_RUN
 //                    if (containerId.isEmpty()) {
 //                        echo "Container $CONTAINER_NAME not found. Building and deploying..."
 //                        sh "docker-compose up -d --build"
