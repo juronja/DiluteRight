a77a07e2b9b875b3bb4b3e7572d367d244b69662
juronja <101410098+juronja@users.noreply.github.com>
2024-10-02 18:22:41 +0200

    added bracket

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,6 +1,7 @@
 def buildEC2() {
     // Check if container exists
     echo "piza" && echo "donuth" && echo "lala"
+    }
     //sh "ssh -o StrictHostKeyChecking=no ec2-user@35.157.110.150 $DOCKER_RUN"
 
 //                    def containerId = sh(script: "docker ps --quiet --filter name=$CONTAINER_NAME", returnStdout: true).trim()
@@ -13,7 +14,5 @@ def buildEC2() {
 //                        sh "docker-compose down" // Stop and remove existing containers/networks (if defined in docker-compose.yml)
 //                        sh "docker-compose up -d" // Start/restart services with latest image
 //                    }
-
-}
 
 return this
