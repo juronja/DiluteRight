36aea00ccfbe9b03e56cdd2f234d82e65cf3e0e0
juronja <101410098+juronja@users.noreply.github.com>
2024-10-03 13:06:52 +0200

    echo container name

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,21 +1,23 @@
 def buildEC2() {
     // Check if container exists
-    echo "something"
-    echo "another thing"
-    //def containerId = sh(script: "docker ps --quiet --filter name=$CONTAINER_NAME", returnStdout: true).trim()
+    echo "$CONTAINER_NAME"
 
-    }
-    
-
-
-//$DOCKER_RUN
-//                    if (containerId.isEmpty()) {
-//                        echo "Container $CONTAINER_NAME not found. Building and deploying..."
-//                        sh "docker-compose up -d --build"
-//                    } else {
-//                        echo "Container $CONTAINER_NAME already exists. Stopping and restarting with latest image..."
-//                        sh "docker-compose down" // Stop and remove existing containers/networks (if defined in docker-compose.yml)
-//                        sh "docker-compose up -d" // Start/restart services with latest image
-//                    }
+//    def containerId = sh(script: "docker ps --quiet --filter name=$CONTAINER_NAME", returnStdout: true).trim()
+//
+//    if (containerId.isEmpty()) {
+//        echo "Container $CONTAINER_NAME not found. Skipping stop/remove steps."
+//    } else {
+//        echo "Stopping and removing existing container $CONTAINER_NAME ..."
+//        sh "docker stop $CONTAINER_NAME"
+//        sh "docker rm $CONTAINER_NAME"
+//        sh "docker rmi $DOCKERH_REPO/$IMAGE_TAG:latest" // Remove leftover image if needed
+//        sh "docker rmi $DOCKERH_REPO/$IMAGE_TAG:$BUILD_VERSION" // Remove leftover image if needed
+//    }
+//
+//    // Always run the container regardless of previous existence
+//    echo "Starting container $CONTAINER_NAME ..."
+//    sh "$DOCKER_RUN"
+//    sh "docker image prune --force"
+}
 
 return this
