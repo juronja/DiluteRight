3d9f00be124c512a26e1e9d6a52fdb6eca904bac
juronja <101410098+juronja@users.noreply.github.com>
2024-10-04 11:59:05 +0200

    comment correction

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Downloading the compose file
+# Downloading and overwriting the compose file
 wget -O compose.yaml https://raw.githubusercontent.com/juronja/DiluteRight/refs/heads/main/compose.yaml
 
 # Stoping and starting the container
 docker-compose down
-docker image prune --force
+#docker image prune --force
 docker-compose up -d
