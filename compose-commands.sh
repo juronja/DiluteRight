ec714b13971788d48bb85ae9dac5fded5d13d3c5
juronja <101410098+juronja@users.noreply.github.com>
2024-10-31 15:30:43 +0100

    remove also unused images

--- compose-commands.sh
+++ compose-commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Stoping the container
-docker compose down
-docker image prune --force
+docker image prune --all --force
+# docker system prune --all --force
 
 # Downloading and overwriting the compose file
 wget -O compose.yaml https://raw.githubusercontent.com/juronja/DiluteRight/refs/heads/main/compose.yaml
 
 # Starting the container
-docker compose up -d
+docker compose up -d --pull always
